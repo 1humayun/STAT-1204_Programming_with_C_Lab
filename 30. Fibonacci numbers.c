8458279e13c7feee67d918f9c78dbdd3bc37a9ca
Humayun Kabir <mdhumayunkabir203@gmail.com>
2023-11-05 02:07:29 +0600

    Add files via upload

---
+++ 30. Fibonacci numbers.c
@@ -0,0 +1,27 @@
+
+//C program for Fibonacci numbers...
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 0; i < n; i++) {
+        if (i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+
+    printf("\n");
+
+    return 0;
+}
